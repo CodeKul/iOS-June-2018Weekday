29d574b2773b30b0718a21a4137fb3e2e55a3344
Varun <varun.kudalkar@gmail.com>
2018-06-26 13:25:54 +0530

    Tabbar controller and Navigation controller

---
+++ NavigationControllerDemo/NavigationControllerDemo/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  NavigationControllerDemo
+//
+//  Created by Varun on 26/06/18.
+//  Copyright © 2018 Codekul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("SecondViewController : viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("SecondViewController : viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("SecondViewController : viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("SecondViewController : viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondViewController : viewDidDisappear")
+    }
+
+}
---
+++ NavigationControllerDemo/NavigationControllerDemo/ThirdViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThirdViewController.swift
+//  NavigationControllerDemo
+//
+//  Created by Varun on 26/06/18.
+//  Copyright © 2018 Codekul. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ThirdViewController : viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ThirdViewController : viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ThirdViewController : viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ThirdViewController : viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ThirdViewController : viewDidDisappear")
+    }
+}
